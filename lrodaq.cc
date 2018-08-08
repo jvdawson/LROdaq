a30c7d123816fbf3a47f68339c71ed50434a4700
jvdawson <jdawson@in2p3.fr>
2018-08-08 10:27:42 +0200

    add lro test code

---
+++ lrodaq.cc
@@ -0,0 +1,19 @@
+#include "card.h"
+//testing version for real...
+int main(void)
+{
+  card mycard("127.16.4.13");
+  bool res=false;
+  res = mycard.isReady(); 
+  /* bool card::SetControlRegisters(uint16_t STrace_Num_Trig,
+			       bool Sstart_enable, 
+			       bool Srem_log_msg_enable, 
+			       bool Ssoft_reboot,
+			       uint32_t Spre_trig_samples,
+			       uint32_t Supdport, //not sure
+			       uint32_t Snbevents)
+  */
+  //in progress 
+  //  res = mycard.SetControlRegisters(200,1,0,0,100,325,100);
+  return 0;
+}
