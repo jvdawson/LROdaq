17c3d464e4f85634496e2b26ce454c2e8a23d761
jvdawson <jdawson@in2p3.fr>
2018-08-09 09:34:38 +0200

    mod server

--- server.cc
+++ server.cc
@@ -32,19 +32,25 @@ void server::init()
 {
   struct sockaddr_in serv_addr;
   
+ if ( (sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror( "socket failed" );
+	// return ;
+    }
 
-  memset(&serv_addr, '0', sizeof(serv_addr));
-  bool res=false;
-  if(debug)std::cout<<"create socket"<<std::endl;
-  res = create_socket();
-  if(debug)std::cout<<"attach socket"<<std::endl;
-  res=attach_socket();
-  if(debug)std::cout<<"connect socket"<<std::endl;
-  res = connect_socket(serv_addr);
-  if(debug)std::cout<<"init done"<<std::endl;
+    struct sockaddr_in serveraddr;
+    memset( &serveraddr, 0, sizeof(serveraddr) );
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons( PORT );
+    serveraddr.sin_addr.s_addr = htonl( INADDR_ANY );
+
+    if ( bind(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0 ) {
+        perror( "bind failed" );
+
+    }
+    //return something?
   
 }
-bool server::create_socket()
+/*bool server::create_socket()
 {
   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {//SOCK_DGRAM? -- SOCK_STREAM [previous]
@@ -98,6 +104,7 @@ bool server::connect_socket(struct sockaddr_in serv_addr)
 
   return true;
 }
+*/
 
 bool server::server_send(unsigned char buffer[], int blen)
 {
@@ -112,19 +119,24 @@ bool server::server_send(unsigned char buffer[], int blen)
 	  }
 	std::cout<<std::endl;
       }
+    //ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
+    //                 const struct sockaddr *dest_addr, socklen_t addrlen);
 
-    send(sock, buffer,blen,0);
-  return true;
-    }
-    return false;
+
+    sendto(sock, buffer,blen,0, NULL, 0);
+    return true;
+  }
+  return false;
 }
 bool server::server_read(unsigned char buffer[]) //how much to read?
 { //need to return the number of bytes read... (more useful)
   std::cout<<sizeof(buffer)<<std::endl;
-  int valread = read(sock, buffer, sizeof(buffer));
+  // int valread = read(sock, buffer, sizeof(buffer),0,NULL,0);
+  int length = recvfrom( sock, buffer, sizeof(buffer) - 1, 0, NULL, 0 );
+       
   if(debug){
-    std::cout<<"valread "<<valread<<std::endl;
-    for(int i=0;i<valread;i++)
+    std::cout<<"length:  "<<length<<std::endl;
+    for(int i=0;i<length;i++)
       { //this is passed as a char, but will be decoded as unsigned char 
 	std::cout<<std::hex<<unsigned(buffer[i])<<" ";
       }
--- server.h
+++ server.h
@@ -17,10 +17,10 @@ class server
  private:
   int PORT;
   int sock;
-  int new_socket;
+  // int new_socket;
   
   void init();
-  bool create_socket();
-  bool connect_socket(struct sockaddr_in serv_addr);//point-to-point I thought?
-  bool attach_socket();
+  //  bool create_socket();
+  // bool connect_socket(struct sockaddr_in serv_addr);//point-to-point I thought?
+  // bool attach_socket();
 };
